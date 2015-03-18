c84bdb3fc77cc2c69aa2c1ea16abefb563392eb9
mentoranonymous <wplanchez.ing.mecatronico@hotmail.com>
2015-03-18 12:19:43 +0100

    VCsharp
    Switch

---
+++ Program_04_Switch/Program_04_Switch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Program_04_Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letra; // String letra
+            Console.Write("Introduce una letra (a ó b): ");
+            letra = Console.ReadKey().KeyChar; // letra = Console.ReadLine()
+            Console.WriteLine();
+
+            switch (letra) { // INicio del Switch
+                case 'a': {
+                    Console.WriteLine("Haz presisonado la letra 'a'.");
+                    break;
+                }
+
+                case 'b': {
+                    Console.WriteLine("Haz presisonado la letra 'b'.");
+                    break;
+                }
+            }   // Fin del Switch
+            Console.ReadKey();
+        }
+    }
+}
