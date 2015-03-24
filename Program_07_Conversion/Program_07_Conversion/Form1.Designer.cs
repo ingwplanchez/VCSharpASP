c3e5dc9f91cf17917708b62b69fcadaba6a87a98
mentoranonymous <wplanchez.ing.mecatronico@hotmail.com>
2015-03-24 10:41:11 +0100

    VCSharpASP
    Tutorial VCSharp ASP

---
+++ Program_07_Conversion/Program_07_Conversion/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Program_07_Conversion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textCent = new System.Windows.Forms.TextBox();
+            this.textFahr = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Grados Centigrados";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Grados Fahrenheit";
+            // 
+            // textCent
+            // 
+            this.textCent.Location = new System.Drawing.Point(116, 13);
+            this.textCent.Name = "textCent";
+            this.textCent.Size = new System.Drawing.Size(100, 20);
+            this.textCent.TabIndex = 2;
+            // 
+            // textFahr
+            // 
+            this.textFahr.Location = new System.Drawing.Point(116, 42);
+            this.textFahr.Name = "textFahr";
+            this.textFahr.ReadOnly = true;
+            this.textFahr.Size = new System.Drawing.Size(100, 20);
+            this.textFahr.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Convertir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 135);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textFahr);
+            this.Controls.Add(this.textCent);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textCent;
+        private System.Windows.Forms.TextBox textFahr;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Program_07_Conversion/Program_07_Conversion/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Program_07_Conversion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float cent = float.Parse(this.textCent.Text);
+            float fahr = (cent * 9 / 5) + 32;
+            this.textFahr.Text = fahr.ToString();
+        }
+    }
+}
